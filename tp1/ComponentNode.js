86e83bf41081c2bee5098bcf4cffc5921294fc60
Filipe Campos <filipepcampos24@gmail.com>
2022-09-26 15:34:44 +0100

    Add documentation for ComponentNode.js and SceneData.js

--- tp1/ComponentNode.js
+++ tp1/ComponentNode.js
@@ -1,5 +1,14 @@
+/**
+ * Component Node representation, contains data from <component>
+ * @constructor
+ * @param id - Node id
+ * @param transformationId - Id of the transformation used
+ * @param materialId - Id of the material used (Or "inherit")
+ * @param textureId - Id of the texture used (Or "inherit" / "none")
+ * @param childComponentsId - Ids of all the children components
+ * @param childPrimitivesId - Ids of all the children primitives
+ */
 export class ComponentNode {
-    // TODO: DOCUMENTATION
     constructor(id, transformationId, materialId, textureId, childComponentsId, childPrimitivesId) {
         this.id = id;
         this.transformationId = transformationId;
@@ -11,10 +20,6 @@ export class ComponentNode {
 
     setChildren(children) {
         this.children = children;
-    }
-
-    applyTransformation(sceneData){
-        
     }
 
     display(sceneData){
--- tp1/SceneData.js
+++ tp1/SceneData.js
@@ -1,3 +1,12 @@
+/**
+ * SceneData
+ * @constructor
+ * @param scene - Reference to MyScene object
+ * @param textures - Object with all texture definitions
+ * @param materials - Object with all materials definitions
+ * @param transformations - Object with all transformations definitions
+ * @param primitive - Object with all primitives definitions
+ */
 export class SceneData {
     constructor(scene, textures, materials, transformations, primitives){
         this.scene = scene;
