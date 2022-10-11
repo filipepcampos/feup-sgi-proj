4f73c27fc196727273d27f84d1b6ccc341453af4
Francisco Cerqueira <up201905337@edu.fe.up.pt>
2022-10-11 12:33:22 +0100

    Added SceneData

---
+++ tp1/models/SceneData.js
@@ -0,0 +1,27 @@
+import { CGFappearance } from "../../lib/CGF.js";
+
+/**
+ * SceneData
+ * @constructor
+ */
+export class SceneData {
+    constructor(scene) {
+        // Global values
+        this.scene = scene;
+        this.root = null;
+
+        // Active (default) values
+        this.activeCamera = 0;
+
+        // Lists of elements
+        this.views = {};
+        this.lights = {};
+        this.textures = {};
+        this.materials = {};
+        this.transformations = {};
+
+        this.primitives = {};
+        this.components = {}
+
+    }
+}
