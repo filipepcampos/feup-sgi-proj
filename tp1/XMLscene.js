8bbc380ad4623a30612dd5c1fb568f21a2e9d5ee
Filipe Campos <filipepcampos24@gmail.com>
2022-10-21 17:46:52 +0100

    feat: calculate direction from target and pos

--- tp1/XMLscene.js
+++ tp1/XMLscene.js
@@ -107,7 +107,7 @@ export class XMLscene extends CGFscene {
                 if (light[1] == "spot") {
                     this.lights[i].setSpotCutOff(light[7]);
                     this.lights[i].setSpotExponent(light[8]);
-                    this.lights[i].setSpotDirection(light[9][0], light[9][1], light[9][2]);
+                    this.lights[i].setSpotDirection(light[9][0]-light[2][0], light[9][1]-light[2][1], light[9][2]-light[2][2]);
                 }
 
                 if (light[0])
