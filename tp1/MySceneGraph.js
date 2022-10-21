daa3d415972094a258a9bb1c64d6583733284b2c
Filipe Campos <filipepcampos24@gmail.com>
2022-10-21 19:43:24 +0100

    fix: remove unecessary parseColor function

--- tp1/MySceneGraph.js
+++ tp1/MySceneGraph.js
@@ -370,39 +370,6 @@ export class MySceneGraph {
         return null;
     }
 
-    /**
-     * Parse the color components from a node
-     * @param {block element} node
-     * @param {message to be displayed in case of error} messageError
-     */
-    parseColor(node, messageError) {
-        var color = [];
-
-        // R
-        var r = this.reader.getFloat(node, 'r');
-        if (!(r != null && !isNaN(r) && r >= 0 && r <= 1))
-            return "unable to parse R component of the " + messageError;
-
-        // G
-        var g = this.reader.getFloat(node, 'g');
-        if (!(g != null && !isNaN(g) && g >= 0 && g <= 1))
-            return "unable to parse G component of the " + messageError;
-
-        // B
-        var b = this.reader.getFloat(node, 'b');
-        if (!(b != null && !isNaN(b) && b >= 0 && b <= 1))
-            return "unable to parse B component of the " + messageError;
-
-        // A
-        var a = this.reader.getFloat(node, 'a');
-        if (!(a != null && !isNaN(a) && a >= 0 && a <= 1))
-            return "unable to parse A component of the " + messageError;
-
-        color.push(...[r, g, b, a]);
-
-        return color;
-    }
-
     /*
      * Callback to be executed on any read error, showing an error on the console.
      * @param {string} message
