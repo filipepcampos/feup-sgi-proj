1183df43291ae2ed9138509078df912d131c9f29
Francisco Cerqueira <up201905337@edu.fe.up.pt>
2022-11-22 18:02:34 +0000

    docs: start working on documentation

--- tp2/models/Keyframe.js
+++ tp2/models/Keyframe.js
@@ -1,4 +1,11 @@
+/**
+ * Class that represents a keyframe
+ */
 export class Keyframe {
+    /**
+     * @param {number} instant - Instant of the keyframe
+     * @param {*} transformation - Transformation matrix of the keyframe
+     */
     constructor(instant, transformation) {
         this.instant = instant;
         this.transformation = transformation;
