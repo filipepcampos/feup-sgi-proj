837aba4cc78400142a24f708c4c8e03a099a8866
Francisco Cerqueira <up201905337@edu.fe.up.pt>
2022-12-28 21:59:04 +0000

    docs: add documentation for game models

--- tp3/game/models/MyGameBoard.js
+++ tp3/game/models/MyGameBoard.js
@@ -4,13 +4,22 @@ import { MyTile } from "./MyTile.js";
 const NUM_ROWS = 8;
 const NUM_COLS = 8;
 
+/**
+ * Model that represents the game board
+ */
 export class MyGameBoard {
+    /**
+     * @param {*} scene - Reference to MyScene object
+     */
     constructor(scene) {
         this.scene = scene;
         this.setupBoard();
         this.setupPieces();
     }
 
+    /**
+     * Initializes the board
+     */
     setupBoard() {
         this.board = [];
         for (let i = 0; i < NUM_ROWS; ++i) {
@@ -22,6 +31,9 @@ export class MyGameBoard {
         }
     }
 
+    /**
+     * Initializes the pieces
+     */
     setupPieces() {
         this.pieces = [];
         for (let i = 0; i < NUM_COLS/2; i++){
@@ -43,15 +55,29 @@ export class MyGameBoard {
         }        
     }
 
+    /**
+     * Gets the pieces for a player
+     * @param {Number} playerId - Player id
+     * @returns {Array} - Pieces
+     */
     getPiecesByPlayer(playerId) {
         return this.pieces.filter(piece => piece.playerId == playerId);
     }
 
+    /**
+     * Adds a piece to a tile
+     * @param {MyPiece} piece - Piece
+     * @param {MyTile} tile - Tile
+     */
     addPiece(piece, tile) {
         tile.setPiece(piece);
         piece.setTile(tile);
     }
     
+    /**
+     * Removes a piece from a tile
+     * @param {MyTile} tile - Tile
+     */
     removePiece(tile) {
         const piece = tile.piece;
         if (piece) {
@@ -60,16 +86,32 @@ export class MyGameBoard {
         tile.clearPiece();
     }
 
+    /**
+     * Removes a piece from the board pieces list
+     * @param {MyPiece} piece - Piece
+     * @returns {Array} - Pieces
+     */
     removeFromPlay(piece) {
         this.pieces = this.pieces.filter(p => p != piece);
     }
 
+    /**
+     * Moves a piece to a destination tile
+     * @param {MyPiece} piece - Piece
+     * @param {MyTile} destinationTile - Destination tile
+     */
     movePiece(piece, destinationTile) {
         const startingTile = piece.tile;
         this.removePiece(startingTile);
         this.addPiece(piece, destinationTile);
     }
 
+    /**
+     * Gets the tile at a given position
+     * @param {Number} row - Row
+     * @param {Number} col - Column
+     * @returns {MyTile} - Tile
+     */
     getTile(row, col){
         if(this.board[row]) {
             return this.board[row][col];
--- tp3/game/models/MyTile.js
+++ tp3/game/models/MyTile.js
@@ -1,4 +1,12 @@
+/**
+ * Model that represents a game tile
+ */
 export class MyTile {
+    /**
+     * @param {*} scene - Reference to MyScene object
+     * @param {Number} col - Column
+     * @param {Number} row - Row
+     */
     constructor(scene, col, row) {
         this.scene = scene;
         this.row = row;
@@ -6,14 +14,25 @@ export class MyTile {
         this.piece = null;
     }
 
+    /**
+     * Checks if the tile has a piece on it
+     * @returns {Boolean} True if the tile has a piece on it, false otherwise
+     */
     isOccupied() {
         return this.piece != null;
     }
 
+    /**
+     * Sets the piece on the tile
+     * @param {MyPiece} piece - Piece
+     */
     setPiece(piece) {
         this.piece = piece;
     }
 
+    /**
+     * Clears the piece on the tile
+     */
     clearPiece() {
         this.piece = null;
     }
